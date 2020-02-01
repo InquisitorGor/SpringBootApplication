9e6390c9abeb929f6068d61b6465295bb26a453b
InquisitorGor <egorayubdzhanov@gmail.com>
2020-02-01 17:53:30 +0300

    added extensions

--- src/main/java/com/example/demo/repositories/AuthorRepository.java
+++ src/main/java/com/example/demo/repositories/AuthorRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repositories;
 
-public interface AuthorRepository {
+import com.example.demo.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+
 }
--- src/main/java/com/example/demo/repositories/BookRepository.java
+++ src/main/java/com/example/demo/repositories/BookRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repositories;
 
-public interface BookRepository {
+import com.example.demo.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+
 }
