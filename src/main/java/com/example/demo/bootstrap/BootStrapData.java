6515c1a16d3d6e95d10286bda3f34d09ddc53ff8
InquisitorGor <egorayubdzhanov@gmail.com>
2020-02-02 15:08:14 +0300

    added BootStrapData for adding some info to my h2database added Publisher class

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.example.demo.bootstrap;
 
 import com.example.demo.model.Author;
 import com.example.demo.model.Book;
+import com.example.demo.model.Publisher;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.BookRepository;
+import com.example.demo.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +13,16 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123","O'Really");
         eric.getBooks().add(ddd);
@@ -32,10 +36,18 @@ public class BootStrapData implements CommandLineRunner {
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        Publisher pub = new Publisher();
+        pub.setName("PublisherProduction");
+        pub.setCity("Moscow");
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(pub);
+
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Publishers: " + publisherRepository.count());
     }
 }
